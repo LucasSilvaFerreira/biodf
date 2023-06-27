a0d6624599a55579e369aba62a240ff21be44ae9
Lucas Ferreira da Silva <LucasSilvaFerreira@users.noreply.github.com>
2023-06-27 12:57:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="biodf",
     version="0.0.1",
     py_modules=["biodf", ],
-    install_requires=["pandas", "seaborn", "numpy"],
+    install_requires=["pandas", "seaborn", "numpy", "pybedtools"],
 )
